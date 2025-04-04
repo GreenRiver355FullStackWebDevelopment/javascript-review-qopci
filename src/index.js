313e0897e93ecd405fa6db32eef46b8f94de3068
Diana <qopci@DESKTOP-MC25DP2.>
2025-04-03 19:24:48 -0700

    Finished the first deliverable for the Lab, and tested it in the browser!

--- src/index.js
+++ src/index.js
@@ -23,3 +23,42 @@ const reviews = [
 /////////////////////////////////////////////////////////////////////
 
 //Your Code Below Here////
+import { calculateStarAverage } from './logic.js'; 
+
+function renderReview(review) {
+  // main content container
+  const reviewContainer = document.createElement('div');
+  reviewContainer.classList.add('review_container'); 
+
+  // image element
+  const img = document.createElement('img'); 
+  img.src = review.image; 
+  reviewContainer.appendChild(img);
+
+  // second div container
+  const details = document.createElement('div');
+
+  // username
+  const username = document.createElement('p');
+  username.textContent = review.username;
+  details.appendChild(username);
+
+  // star rating
+  const starRating = document.createElement('p');
+  starRating.textContent = `${review.star} stars`; 
+  details.appendChild(starRating);
+
+  // review 
+  const reviewText = document.createElement('p');
+  reviewText.textContent = review.review; 
+  details.appendChild(reviewText);
+
+  // appending the containers together
+  reviewContainer.appendChild(details);
+
+  // appending the dynamically created content within the HTML reviews sections
+  document.querySelector('.reviews').appendChild(reviewContainer);
+}
+
+// rendering each review and looping through it
+reviews.forEach(renderReview);
